718db4bd1b13e2d024a239af827df1ba98027046
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2024-04-18 19:53:03 +0500

    created User(class) Entity

--- src/main/java/com/blog/BlogAppApisApplication.java
+++ src/main/java/com/blog/BlogAppApisApplication.java
@@ -3,11 +3,14 @@ package com.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.blog.entities.User;
+
 @SpringBootApplication
 public class BlogAppApisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogAppApisApplication.class, args);
+		
 	}
 
 }
