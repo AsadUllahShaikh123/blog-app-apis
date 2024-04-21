8d9d5b5e097c17bd24f1c006865854f09853ff4d
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2024-04-21 22:16:13 +0500

    added comment

--- src/main/java/com/blog/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/blog/exceptions/ResourceNotFoundException.java
@@ -16,7 +16,6 @@ public class ResourceNotFoundException extends RuntimeException{
 	private String fieldName;
 	private long fieldValue;
 	// it will be called when there will be raised any exception 
-	
 	public ResourceNotFoundException(String resourceName,String fieldName,long fieldValue) {
 		super(String.format("%s not found with %s : %s", resourceName,fieldName,fieldValue));
 		this.resourceName = resourceName;
