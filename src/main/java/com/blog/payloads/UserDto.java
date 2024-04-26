7fa3bd76e08bab3ed04f9a520915fb1ed55862da
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2024-04-26 19:53:36 +0500

    applied the @Size on fields

--- src/main/java/com/blog/payloads/UserDto.java
+++ src/main/java/com/blog/payloads/UserDto.java
@@ -4,6 +4,7 @@ package com.blog.payloads;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -11,10 +12,12 @@ public class UserDto {
 	
 	private int id;
 	@NotEmpty
+	@Size(min= 4, message="Username must contain atleast 4 characters")
 	private String name;
-	@Email
+	@Email(message="Email address is not valid! ")
 	private String email;
 	@NotNull
+	@Size(min = 3 , max = 10 , message ="Username must contain 3 characters and shouldn't exceed 10")
 	private String password;
 	@NotNull
 	private String about;
