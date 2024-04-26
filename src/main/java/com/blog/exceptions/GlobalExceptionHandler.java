270ed8b390c538b4606fdabcf465933b6b5d46aa
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2024-04-26 20:17:25 +0500

    completed data validation with handling exception.

--- src/main/java/com/blog/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/blog/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,12 @@
 package com.blog.exceptions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,6 +20,18 @@ public class GlobalExceptionHandler {
 		String message = ex.getMessage();
 		ApiResponse response = new ApiResponse(message,false);
 		return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex ){
+		Map<String , String > resp = new HashMap<String,String>();
+		ex.getBindingResult().getAllErrors().forEach(error -> {
+			String fieldName = ((FieldError)error).getField();
+			String errorMessage = error.getDefaultMessage();
+			
+			resp.put(fieldName, errorMessage);
+		});
+		return new ResponseEntity<Map<String,String>>(resp, HttpStatus.BAD_REQUEST);
 	}
 	
 }
