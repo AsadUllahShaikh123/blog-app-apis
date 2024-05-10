9dbd8bb832cd57b85e0e7091671d97eda1b92ba7
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2024-05-10 23:19:18 +0500

    recomended way to use constants

--- src/main/java/com/blog/controllers/PostController.java
+++ src/main/java/com/blog/controllers/PostController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.blog.config.AppConstants;
 import com.blog.payloads.ApiResponse;
 import com.blog.payloads.PostDto;
 import com.blog.payloads.PostResponse;
@@ -43,9 +44,9 @@ public class PostController {
 	}
 	@GetMapping("/posts")
 	public ResponseEntity<PostResponse> getAllPosts(
-			@RequestParam(value="pageNumber",defaultValue="0",required=false) Integer pageNumber,
-			@RequestParam(value="pageSize",defaultValue="10",required=false) Integer pageSize,
-			@RequestParam(value="sortBy",defaultValue="postId",required=false) String sortBy
+			@RequestParam(value="pageNumber",defaultValue=AppConstants.PAGE_NUMBER,required=false) Integer pageNumber,
+			@RequestParam(value="pageSize",defaultValue=AppConstants.PAGE_SIZE,required=false) Integer pageSize,
+			@RequestParam(value="sortBy",defaultValue=AppConstants.SORT_BY,required=false) String sortBy
 			){
 		PostResponse posts =  postService.getAllPosts(pageNumber, pageSize,sortBy);
 		
